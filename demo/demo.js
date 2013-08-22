7f29bccba1eb54337052bbc29c8b8125482bca1c
unknown <support@firstvector.org>
2013-08-22 17:45:10 +0300

    fix for demo

--- demo/demo.js
+++ demo/demo.js
@@ -2,6 +2,8 @@
 	'use strict';
 
 	$(document).tap(function(e){
+		e = e.pageX ? e : e.originalEvent.changedTouches[0];
+
 		$('section').html('Event: '+ e.type +'. Coordinates[x: '+ e.pageX +', y: '+ e.pageY +']');
 	});
 })(jQuery);
